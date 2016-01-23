97a0e4e9220455fe6305c29bf83559b24f7289ea
Nax <max.bacoux@gmail.com>
2016-01-23 22:10:51 +0100

    Tag

--- Sources/Request.swift
+++ Sources/Request.swift
@@ -1,3 +1,23 @@
-class Request {
+import Foundation
 
+class Request {
+    var verb : String?
+    var scheme : String?
+    var host : String?
+    var path : String?
+    var port : Int?
+
+    var fullpath : String? {
+        set {
+            let str = newValue
+            _fullpath = String(str)
+            let regex = try! NSRegularExpression(pattern: "^(.+?):\\/\\/(.+?)(?::([0-9]{1,5}))?(\\/.*)?$", options: [])
+            let matches = regex.matchesInString(str!, options: [], range: NSRange(location: 0, length: str!.characters.count))
+        }
+        get {
+            return _fullpath
+        }
+    }
+
+    private var _fullpath : String?
 }
