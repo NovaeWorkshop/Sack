df0083011f7a1d04080ce036e4b1074bc6b7be97
Nax <max.bacoux@gmail.com>
2016-01-27 06:07:13 +0100

    Content-Length support in response

--- Sources/Sack/Response.swift
+++ Sources/Sack/Response.swift
@@ -4,15 +4,20 @@ public class Response {
     public var status: Int
     public var headers: Headers
     public var body: [NSData]
+    private var size: Int
 
     public init () {
         status = 200
         headers = Headers()
         body = []
+        size = 0
+        headers.set("Content-Length", "0")
     }
 
     public func write (data: NSData) {
         body.append(data)
+        size += data.length
+        headers.set("Content-Length", String(size))
     }
 
     public func write (string: String) {
