de33187384f85aaac84d2014e2e05a9021f2adb3
Nax <max.bacoux@gmail.com>
2016-01-27 08:26:56 +0100

    Support for non-GET requests

--- Sources/Sack/Request.swift
+++ Sources/Sack/Request.swift
@@ -4,7 +4,8 @@ public class Request {
     public var path     : String = ""
     public var verb     : String = ""
     public var url      : NSURL
-    public var headers  : Headers = Headers()
+    public var headers  = Headers()
+    public var body     = NSData()
 
     public init (_ aVerb: String, _ aPath: String) {
         verb = aVerb
